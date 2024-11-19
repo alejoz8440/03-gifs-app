97a00a919d4bcd94544b435dce8f73e8a792d3c3
Alejandro Tamayo <alejoz8440@gmail.com>
2024-11-19 09:06:35 -0500

    ajuste barra sidebar

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,5 +1,6 @@
 
  import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
  @Component({
   selector: 'gifs-search-box',
@@ -20,12 +21,15 @@
 
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>
-  constructor() { }
+
+
+  constructor(private gifsService:GifsService) { }
 
   //searchTag(newTag:string){
   searchTag(){
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag})
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value='';
   }
 
 
