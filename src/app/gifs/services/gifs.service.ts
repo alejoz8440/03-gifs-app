de2b0c112b58401630d3d070b954bae4f69a08c7
Alejandro Tamayo <alejoz8440@gmail.com>
2024-11-20 13:55:49 -0500

    finalización seccion 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -39,6 +39,8 @@ export class GifsService {
   private loadLocalStorage():void{
     if(!localStorage.getItem('history'))return;
     this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag( tag: string):void{
